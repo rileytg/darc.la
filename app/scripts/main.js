58509873b66c4a05ecbb3989fe643c0316957525
Riley Guerin <guerin.riley@gmail.com>
2014-03-09 17:40:51 -0700

    logo with clients list that has a hakimel stroll effect

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+
+    var ADDITIONAL_CONTENT_HEIGHT = 60
+    var frameContentHeight =  $('.header').height()
+            + $('.footer').height()
+            + ADDITIONAL_CONTENT_HEIGHT
+
+    var listHeight = $(window).height() - frameContentHeight
+
+    var clientListSelector = 'ul.hack'
+    $(clientListSelector).height(listHeight)
+    stroll.bind( clientListSelector );
+
+})
