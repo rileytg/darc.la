3d5aec645e1f951c0bfd160fd88508405c02f76a
Riley Guerin <guerin.riley@gmail.com>
2014-03-09 20:03:00 -0700

    fix imagemin and add buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -347,7 +347,27 @@ module.exports = function (grunt) {
                 'imagemin',
                 'svgmin'
             ]
-        }
+        },
+        buildcontrol: {
+            options: {
+                dir: 'dist',
+                commit: true,
+                push: true,
+                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            pages: {
+                options: {
+                    remote: ' https://github.com/rileytg/darc.la.git',
+                    branch: 'gh-pages'
+                }
+            },
+            local: {
+                options: {
+                    remote: '../',
+                    branch: 'build'
+                }
+            }
+          }
     });
 
 
